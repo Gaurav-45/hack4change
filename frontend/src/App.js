f8c0b58efc4b31dfd8d10028768ffe749e79c5c7
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-07-29 00:02:28 +0530

    Topnavbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Home from "./components/Home/Home";
+import TopNavbar from "./components/TopNavbar/TopNavbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <TopNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
